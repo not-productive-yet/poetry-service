ea53f115bdacdc98306535423ef8ad86cec6e1ab
Lisa Trovato-Monastra <l.trovato-monastra@posteo.de>
2022-12-06 20:32:19 +0100

    initial poetry randomizer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [poem, setPoem] = useState({});
+
+  useEffect(() => {
+    getRandomPoem();
+  }, []);
+
+  function getRandomPoem() {
+    console.log("hell");
+    axios.get("https://poetrydb.org/random").then(function (response) {
+      console.log(response.data[0].lines);
+      setPoem(response.data[0]);
+    });
+  }
+  if (!poem) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div>
+        <div className="poem-title">{poem.title}</div>
+        <div className="poem-author">{poem.author}</div>
+      </div>
+
+      <div className="poem-lines">
+        {poem.lines ? poem.lines.map((line) => <li>{line}</li>) : ""}
+      </div>
+      <button class="button" onClick={getRandomPoem}>
+        next
+      </button>
     </div>
   );
 }
